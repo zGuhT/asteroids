cd26e42de4f69079cfee47f0efaaeea7b3e7e033
zguht <jvoigt@komatsu.com.au>
2025-05-14 21:23:56 +0800

    B: player added - sweet!

--- main.py
+++ main.py
@@ -7,15 +7,33 @@ import pygame
 # constants.py into the current file
 from constants import *
 
+# import everything from the module
+# player.py into the current file
+from player import *
+
 def main():
     pygame.init()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        updatable.update(dt)
+        for draw in drawable:
+            draw.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
